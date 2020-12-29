30a1ab6a067b8cb60515bd2a0e0a1c199665a220
魏国 <weiguo@zhilunkeji.com>
2020-12-29 16:02:15 +0800

    feat(*): 整理cli文件
    命令对应文件统一放到lib下面

--- bin/we.js
+++ bin/we.js
@@ -5,12 +5,51 @@
  *@author: forguo
  *@date: 2020/12/28
  */
-const commander = require('commander');
+const program = require('commander');
+const chalk = require("chalk");
+const getVersions = require('../lib/util/getVersions');
+console.log(getVersions());
 
-commander
-    .version(require('../package').version)
+program
+    .version(getVersions())
     .usage('<command> [options]')
-    .command('init', 'init a project')
-    .command('create <app-name>', 'create a project')
-    // .description('create a project')
-    .parse(process.argv);
+
+// create a new project
+program
+    .command('create <app-name>')
+    .description('create a new project')
+    .action((name) => {
+        require('../lib/create')(name)
+    })
+
+// generator a new page or component
+program
+    .command('new <module>')
+    .description('generator a new module')
+    .action((name) => {
+        require('../lib/new')(name)
+    });
+
+// start a serve in the current project
+program
+    .command('serve')
+    .description('start a serve in the current project')
+    .action(() => {
+        require('../lib/serve')()
+    });
+
+// output help information on unknown commands
+program
+    .arguments('<command>')
+    .action((cmd) => {
+        program.outputHelp()
+        console.log(`  ` + chalk.red(`Unknown command ${chalk.yellow(cmd)}.`))
+        console.log()
+        process.exitCode = 1
+    })
+
+program.parse(process.argv)
+
+if (!process.argv.slice(2).length) {
+    program.outputHelp()
+}
