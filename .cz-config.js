6bcd95eb5815afef8625613508fbb0bdb7950d0e
魏国 <weiguo@zhilunkeji.com>
2021-01-01 23:58:50 +0800

    feat(accounts): 添加git cz的body，添加汉化配置文件
    添加详细描述参数，添加Git cz中午配置文件

--- .cz-config.js
+++ .cz-config.js
@@ -60,7 +60,7 @@ module.exports = {
     allowCustomScopes: true,
     allowBreakingChanges: ['feat', 'fix'],
     // skip any questions you want
-    skipQuestions: ['body'],
+    // skipQuestions: ['body'],
 
     // limit subject length
     subjectLimit: 100,
